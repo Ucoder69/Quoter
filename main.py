b6973c61ad4d758a226c8053f9845849da99a753
Ucoder <129254529+Ucoder69@users.noreply.github.com>
2026-07-22 22:13:39 +0530

    v1.1

---
+++ main.py
@@ -0,0 +1,8 @@
+from gui.main_window import MainWindow
+import customtkinter as ctk
+
+root = ctk.CTk()
+MainWindow(root)
+root.mainloop()
+
+# Name: John Doe, email: john.doe@example.com. Pencil book room 1 on 25th Dec for 3 nights for 2 adults and child 4 years.
